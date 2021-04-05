a310ae8151a0629d4349029dfb01f96da3e3d8d5
himanshu76200 <himanshu76200@gmail.com>
2021-04-05 23:31:25 +0530

    implemented login/authentication via Spotify API

---
+++ src/spotify.js
@@ -0,0 +1,30 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "467832202ad742a88db84cd5722f66cf";
+
+const scopes = [
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+    "user-read-currently-playing"
+];
+
+
+//pulling ascess token
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            let parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            return initial;
+        }, {});
+}
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+)}&response_type=token&show_dialog=true`;
